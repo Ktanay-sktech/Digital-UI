174494c424da0a2ea168d65783086f54a27174c6
ktanay-sktech <ktanay508@gmail.com>
2019-08-16 22:46:13 +0530

    new version of navbar

--- js/script.js
+++ js/script.js
@@ -108,3 +108,20 @@ $(function() {
 $(function() {
     new WOW().init();
 });
+$(window).on('load', function() {
+    $("#home-heading-1").addClass("animated fadeInDown")
+});
+
+//Sticky navbar
+window.onscroll = function() { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
